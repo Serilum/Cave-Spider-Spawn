f367de9509c9cdd2fd037d09429fca4d53462c72
Rick South <rick@natamus.com>
2024-08-09 08:31:09 +0200

    Cave Spider Spawn source code for 1.21.1.

---
+++ Common/src/main/java/com/natamus/cavespiderspawn/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.cavespiderspawn.util;
+
+public class Reference {
+	public static final String MOD_ID = "cavespiderspawn";
+	public static final String NAME = "Cave Spider Spawn";
+	public static final String VERSION = "1.2";
+	public static final String ACCEPTED_VERSIONS = "[1.21.1]";
+}
